695f4499f2011dd6e68f417a91e780b818008199
Patrick Jensen <patrick.c.jensen@gmail.com>
2015-01-19 17:30:15 -0500

    Move the code for installing drupal into the main file. Makes installing much cleaner.

--- virtualhost.sh
+++ virtualhost.sh
@@ -107,7 +107,25 @@ if [ "$action" == 'create' ]
 		echo -e "Would you like to install drupal? [Y,n]"
 		read install_drupal
 		if [[ $install_drupal == "Y" || $install_drupal == "y" ]]; then
-			source drupal_virtualhost.sh
+			command -v drush >/dev/null 2>&1 || { echo "This script requires drush, but it's not installed.  Aborting." >&2; exit 1; }
+			drush dl drupal --drupal-project-rename=$rootdir --destination=$userDir --yes
+			cd $userDir$rootdir
+
+			while [ "$dbuser" == "" ]
+			do
+				echo -e "Please enter the database username."
+				read dbuser 
+			done
+
+			while [ "$dbpwd" == "" ]
+			do
+				echo -e "Please enter the database password."
+				read -s dbpwd
+			done
+
+			account_pass="admin"
+			drush site-install standard --db-url="mysql://$dbuser:$dbpwd@localhost/$rootdir" --site-name=$rootdir --account-pass=$account_pass --yes
+			echo -e "Drupal installed! \nYou now have a new Drupal site. \nYour new host is: http://"$domain" \nIt's located at $userDir$rootdir"
 		fi
 	else
 		### check whether domain already exists
